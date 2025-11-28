cd6bfd7175d57cd3f8f6340f6fb07c9f1dc31760
AtlanticWL <darknesscombo@gmail.com>
2025-11-28 10:22:32 -0500

    Removed debug log spam

--- Somnium/Assets/Scripts/GhostBrain.cs
+++ Somnium/Assets/Scripts/GhostBrain.cs
@@ -351,7 +351,6 @@ public class GhostBrain : MonoBehaviour
         {
             MoveToTarget(patrol.CurrentTarget.Value, patrolSpeed);
         }
-        Debug.Log("Patrolling");
     }
 
     void SearchTick()
